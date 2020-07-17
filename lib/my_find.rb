d1553cf91f142d9f89c6f970c3f03b039e12ad80
Kinnari Shah <kinnari1022@yahoo.com>
2020-07-17 16:45:39 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,9 +3,16 @@ require 'pry'
 def my_find(collection)
   i = 0 
   while i < collection.length 
+<<<<<<< HEAD
     if yield(collection[i])
       return collection[i]
     end
     i = i + 1 
   end
+=======
+  yield(collection[i])
+  
+  end
+
+>>>>>>> 26152a9004d50a1850c146cdaa7f74177c18893c
 end
